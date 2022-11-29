3b7ea078313a03d439fe07c50670867dce4fc83d
Jag Reehal <jag.reehal@gmail.com>
2022-11-29 22:08:59 +0000

    cleaned up validation using zod

--- src/functions/record-patient-temperature.ts
+++ src/functions/record-patient-temperature.ts
@@ -1,18 +1,20 @@
 import { logger, metrics, tracer } from '@/powertools/utilities';
-import { injectLambdaContext } from '@aws-lambda-powertools/logger';
-import { logMetrics, MetricUnits } from '@aws-lambda-powertools/metrics';
-import { captureLambdaHandler } from '@aws-lambda-powertools/tracer';
+import { createJsonResponse } from '@/utils/create-json-response';
+import { wrapHttpHandler } from '@/utils/wrap-handler';
+import { MetricUnits } from '@aws-lambda-powertools/metrics';
 import {
   EventBridgeClient,
   PutEventsCommand,
 } from '@aws-sdk/client-eventbridge';
 
-import middy from '@middy/core';
 import {
   APIGatewayProxyEvent,
   APIGatewayProxyHandler,
   APIGatewayProxyResult,
 } from 'aws-lambda';
+import createHttpError from 'http-errors';
+
+import { z } from 'zod';
 
 const eventBridgeClient = tracer.captureAWSv3Client(
   new EventBridgeClient({
@@ -20,80 +22,41 @@ const eventBridgeClient = tracer.captureAWSv3Client(
   })
 );
 
+const schema = z.object({
+  body: z.object({
+    temperature: z.number(),
+  }),
+  pathParameters: z.object({
+    id: z.string(),
+  }),
+});
+
 export const lambdaHandler: APIGatewayProxyHandler = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
-  const id = event.pathParameters!.id;
-  if (id === undefined) {
-    logger.warn(
-      "Missing 'id' parameter in path while trying to create a product",
-      {
-        details: { eventPathParameters: event.pathParameters },
-      }
-    );
-
-    return {
-      statusCode: 400,
-      headers: { 'content-type': 'application/json' },
-      body: JSON.stringify({ message: "Missing 'id' parameter in path" }),
-    };
-  }
-
-  if (!event.body) {
-    logger.warn(
-      'Empty request body provided while trying to record a patient temperature'
-    );
-
-    return {
-      statusCode: 400,
-      headers: { 'content-type': 'application/json' },
-      body: JSON.stringify({ message: 'Empty request body' }),
-    };
-  }
-
-  let body;
-  try {
-    body = JSON.parse(event.body);
-    if (typeof body !== 'object') {
-      throw Error('Parsed product is not an object');
-    }
-  } catch (error) {
-    logger.error('Unexpected error occurred while trying to create a product', {
-      error,
+  const validationResult = await schema.safeParseAsync(event);
+  if (!validationResult.success) {
+    logger.warn(validationResult.error.toString(), {
+      details: {
+        pathParameters: event.pathParameters,
+        body: event.body,
+      },
     });
-
-    return {
-      statusCode: 400,
-      headers: { 'content-type': 'application/json' },
-      body: JSON.stringify({
-        message: 'Failed to parse product from request body',
-      }),
-    };
+    throw new createHttpError.BadRequest(validationResult.error.toString());
   }
 
-  const { temperature } = body;
-
-  if (temperature === undefined) {
-    logger.error(`Temperature is a required field ${event.body}`);
-
-    return {
-      statusCode: 400,
-      headers: { 'content-type': 'application/json' },
-      body: JSON.stringify({
-        message: 'temperature is required',
-      }),
-    };
-  }
+  const { id } = validationResult.data.pathParameters;
+  const { temperature } = validationResult.data.body;
 
   metrics.addMetric('temperatureRecorded', MetricUnits.Count, 1);
   metrics.addMetadata('patientId', id);
-  metrics.addMetadata('temperature', temperature);
+  metrics.addMetadata('temperature', temperature.toString());
   tracer.putAnnotation('temperatureRecorded', true);
 
   const putEventsCommand = new PutEventsCommand({
     Entries: [
       {
-        Detail: JSON.stringify({ id, temperature: +temperature }),
+        Detail: JSON.stringify({ id, temperature }),
         DetailType: 'PATIENT_TEMPERATURE_RECORDED',
         EventBusName: process.env.EVENT_BUS_NAME,
         Source: 'event.tracing',
@@ -107,18 +70,12 @@ export const lambdaHandler: APIGatewayProxyHandler = async (
   tracer.putAnnotation('id', id);
   tracer.putAnnotation('temperature', temperature);
 
-  return {
-    statusCode: 201,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
+  return createJsonResponse(
+    {
       message: 'Temperature recorded',
-    }),
-  };
+    },
+    201
+  );
 };
 
-export const handler = middy(lambdaHandler)
-  .use(captureLambdaHandler(tracer))
-  .use(logMetrics(metrics, { captureColdStartMetric: true }))
-  .use(injectLambdaContext(logger, { clearState: true }));
+export const handler = wrapHttpHandler(lambdaHandler);
--- src/functions/store-patient-temperature.ts
+++ src/functions/store-patient-temperature.ts
@@ -5,36 +5,39 @@ import { logger, metrics, tracer } from '@/powertools/utilities';
 import { logMetrics, MetricUnits } from '@aws-lambda-powertools/metrics';
 import middy from '@middy/core';
 import { captureLambdaHandler } from '@aws-lambda-powertools/tracer';
-import { injectLambdaContext } from '@aws-lambda-powertools/logger';
+import { z } from 'zod';
+import createHttpError from 'http-errors';
+import { wrapHandler } from '@/utils/wrap-handler';
+
+const schema = z.object({
+  detail: z.object({
+    id: z.string(),
+    temperature: z.number(),
+  }),
+});
 
 const store: PatientStore = new DynamoDbStore();
 const lambdaHandler: Handler = async (event) => {
   tracer.putMetadata('patientTemperatureRecordedReceived', event);
 
-  const { id, temperature } = event.detail;
-  if (id === undefined || temperature === undefined) {
-    logger.warn('Missing id or temperature', {
-      details: { detail: event.detail },
+  const validationResult = await schema.safeParseAsync(event);
+  if (!validationResult.success) {
+    logger.warn(validationResult.error.toString(), {
+      detail: event.detail,
     });
-
-    return {
-      statusCode: 400,
-      headers: { 'content-type': 'application/json' },
-      body: JSON.stringify({ message: 'Missing id or temperature' }),
-    };
+    throw new createHttpError.BadRequest(validationResult.error.toString());
   }
+
+  const { id, temperature } = validationResult.data.detail;
 
   await store.putPatientTemperature({ id, temperature });
   metrics.addMetric('patientTemperatureStored', MetricUnits.Count, 1);
   metrics.addMetadata('id', id);
-  metrics.addMetadata('temperature', temperature);
+  metrics.addMetadata('temperature', temperature.toString());
   tracer.putAnnotation('id', id);
   tracer.putAnnotation('temperature', temperature);
 
   return;
 };
 
-export const handler = middy(lambdaHandler)
-  .use(captureLambdaHandler(tracer))
-  .use(logMetrics(metrics, { captureColdStartMetric: true }))
-  .use(injectLambdaContext(logger, { clearState: true }));
+export const handler = wrapHandler(lambdaHandler);
